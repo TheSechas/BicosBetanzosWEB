40c5fa90f6252549368a40d80832c5ddeebab34d
Sergio González García <sergiogg20012003@gmail.com>
2024-06-30 13:53:05 +0200

    Update script

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const days = ['lunes', 'martes', 'miércoles', 'jueves', 'viernes', 'sábado', 'domingo'];
-    const today = new Date().getDay()-1;
+    var today;
+    if (new Date().getDay() == 0) {
+        today = 6;
+    } else {
+        today = new Date().getDay() - 1;
+    }
     const scheduleItems = document.querySelectorAll('#schedule li');
-
     scheduleItems.forEach((item, index) => {
         if (index === today) {
             item.innerHTML = 'Hoy: ' + item.innerHTML.split(': ')[1];
